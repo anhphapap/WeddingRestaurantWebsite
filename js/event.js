95de750d05a700eb783fefcb8b4e877941867c28
Anh Pha <anhphapap0@gmail.com>
2023-07-15 11:38:48 +0700

    Ver 1

---
+++ js/event.js
@@ -0,0 +1,76 @@
+window.onload = () => {
+  // On Title Hover
+  let onHover = document.getElementById("nav-event");
+  onHover.classList.add("onHover");
+  //On Toggle Menu
+  let menu = document.querySelectorAll("#menu-icon");
+  let toggle = document.querySelector(".toggle-menu");
+  for (let m of menu) {
+    m.addEventListener("click", () => {
+      toggle.classList.toggle("active");
+      document.body.classList.toggle("fixed");
+      m.classList.toggle("bx-menu");
+      m.classList.toggle("bx-x");
+    });
+  }
+  //Data event
+  let listEvent = document.querySelector(".list-event");
+  fetch("../data/event.json")
+    .then((res) => res.json())
+    .then((data) => {
+      for (let e of data) {
+        let event = `<div class="event-item">
+    <div class="event-content">
+        <h1 class="event-title">${e.title}</h1>
+        <div class="event-line"></div>
+        <p class="event-disc">${e.description}</p>
+    </div>
+    <div class="event-img">
+        <div>
+            <img src="${e.image}" alt="" class="event-image">
+        </div>
+    </div>
+</div>`;
+        listEvent.insertAdjacentHTML("beforeend", event);
+      }
+    });
+  //Hide nav
+  const nav = document.querySelector(".nav-bar");
+  const nav1 = document.querySelector(".nav-bar1");
+  if (window.pageYOffset >= 10) {
+    nav.classList.remove("hide");
+    nav1.classList.add("hide");
+  } else {
+    nav.classList.add("hide");
+    nav1.classList.remove("hide");
+  }
+  window.addEventListener(
+    "scroll",
+    debounceFn(function (e) {
+      const scrollY = window.pageYOffset;
+      if (scrollY >= 10) {
+        nav.classList.remove("hide");
+        nav1.classList.add("hide");
+      } else {
+        nav.classList.add("hide");
+        nav1.classList.remove("hide");
+      }
+    }, 10)
+  );
+  // 2. debounce: là kỹ thuật buộc một hàm phải đợi một khoảng thời gian nhất định trước khi thực thi. Trong khoản thời gian đợi, mọi tác động sẽ đều bị bỏ qua và chỉ nhận duy nhất 1 hành động diễn ra trong thời gian chúng ta định trước.
+  function debounceFn(func, wait, immediate) {
+    let timeout;
+    return function () {
+      let context = this,
+        args = arguments;
+      let later = function () {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      let callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    };
+  }
+};
---
+++ js/hall.js
@@ -0,0 +1,78 @@
+// window.onload = () => {
+// On Title Hover
+let onHover = document.getElementById("nav-hall");
+onHover.classList.add("onHover");
+
+//On Toggle Menu
+let menu = document.querySelectorAll("#menu-icon");
+let toggle = document.querySelector(".toggle-menu");
+for (let m of menu) {
+  m.addEventListener("click", () => {
+    toggle.classList.toggle("active");
+    document.body.classList.toggle("fixed");
+    m.classList.toggle("bx-menu");
+    m.classList.toggle("bx-x");
+  });
+}
+
+//Hall effect
+let x = window.innerWidth;
+if (x > 768) {
+  let hallEffect = document.querySelectorAll(".hall-item");
+  hallEffect[2].classList.add("active-hall");
+  for (let i = 0; i < hallEffect.length; i++) {
+    hallEffect[i].addEventListener("click", () => {
+      if (hallEffect[i].classList.contains("active-hall")) {
+        hallEffect[i].classList.remove("active-hall");
+      } else {
+        for (let j = 0; j < hallEffect.length; j++) {
+          if (i !== j && hallEffect[j].classList.contains("active-hall")) {
+            hallEffect[j].classList.remove("active-hall");
+          }
+        }
+        hallEffect[i].classList.add("active-hall");
+      }
+    });
+  }
+}
+
+//Hide nav
+const nav = document.querySelector(".nav-bar");
+const nav1 = document.querySelector(".nav-bar1");
+if (window.pageYOffset >= 10) {
+  nav.classList.remove("hide");
+  nav1.classList.add("hide");
+} else {
+  nav.classList.add("hide");
+  nav1.classList.remove("hide");
+}
+window.addEventListener(
+  "scroll",
+  debounceFn(function (e) {
+    const scrollY = window.pageYOffset;
+    if (scrollY >= 10) {
+      nav.classList.remove("hide");
+      nav1.classList.add("hide");
+    } else {
+      nav.classList.add("hide");
+      nav1.classList.remove("hide");
+    }
+  }, 10)
+);
+// 2. debounce: là kỹ thuật buộc một hàm phải đợi một khoảng thời gian nhất định trước khi thực thi. Trong khoản thời gian đợi, mọi tác động sẽ đều bị bỏ qua và chỉ nhận duy nhất 1 hành động diễn ra trong thời gian chúng ta định trước.
+function debounceFn(func, wait, immediate) {
+  let timeout;
+  return function () {
+    let context = this,
+      args = arguments;
+    let later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    let callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
+// };
