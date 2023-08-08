1274382f69e704da8be86ad13b737fd627f83c70
Anh Pha <anhphapap0@gmail.com>
2023-08-08 20:26:59 +0700

    Home Slider

--- js/script.js
+++ js/script.js
@@ -105,4 +105,33 @@ $(window).ready(()=>{
     $(".select").parent().css("color","var(--text)");
     $(this).parent().css("color","var(--text2)");
   })
+  let l=$(".slider>img").length;
+    let k="";
+    for(let i=0;i<l;i++)
+        k+=`<div class="btn-circle"></div>`
+    $(".list-btn").html(k);
+    $(".slider>img").eq(0).addClass("active-img")
+    $(".list-btn>div").eq(0).addClass("active-btn")
+    let current=0;
+    function setLoop(){
+        timer=setInterval(()=>{
+            current++;
+            if(current==l)
+                current=0;
+            $(".slider>img").removeClass("active-img")
+            $(".slider>img").eq(current).addClass("active-img")
+            $(".list-btn>div").removeClass("active-btn")
+            $(".list-btn>div").eq(current).addClass("active-btn")
+        },3000);
+    }
+    setLoop();
+    $(".list-btn>div").click(function(){
+        current=$(this).index();
+        $(".slider>img").removeClass("active-img")
+        $(".slider>img").eq(current).addClass("active-img");
+        $(".list-btn>div").removeClass("active-btn")
+        $(this).addClass("active-btn")
+        clearInterval(timer);
+        setLoop();
+    })
 });
